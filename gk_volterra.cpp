1e8cd7b12ce2b7ddae30430eb5f4543c7a6fd4e5
Ian Abel <iabel@umd.edu>
2024-07-08 14:54:32 -0400

    sync

---
+++ gk_volterra.cpp
@@ -0,0 +1,28 @@
+
+#include "Volterra.hpp"
+#include "GK.hpp"
+
+#include <cmath>
+#include <iostream>
+#include <fstream>
+
+
+int main( int, char ** )
+{
+	unsigned int N = 4096;
+	double tEnd = 1500.0;
+	SimpsonSolver< std::complex<double> > ssolve;
+	LinearGK lin_gk( 1.0, 50.0, 2.0, 0.0, 1.0, 1.0 );
+	auto Source = lin_gk.getSourceFn();
+	auto Kernel = lin_gk.getKernelFn();
+	auto data = ssolve.Solve( Source, Kernel, 1.0, N );
+
+	std::cout << "# t\tPhi^2" << std::endl;
+	
+	for( unsigned int i=0; i <= N; ++i )
+	{
+		double ti = i * tEnd / N;
+		std::cout << ti << '\t' << std::norm(data[i]) << std::endl;
+	}
+	return 0;
+}
